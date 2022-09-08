64e267e9d28e066de6a61388ccd1f04edc478c75
Umohjoy <umohjoy27@gmail.com>
2022-09-08 16:39:53 -0700

    new

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,6 +2,7 @@
 /**
  * main - main block
  * Description: using the main function
+ * this program prints "Programming is like building a multilingual puzzle
  * Return 0 if success.
  */
 int main(void)
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
+
 /**
  * main - print string in the function
- * Return: 0
+ * but the outcome is an art piece
+ * Description: using the main function
+ * this program prints "programming is like building a multiligual puzzle
+ * Return 0 if exited properly, non zero otherwise
  */
 int main(void)
 {			
-prinf("with proper grammar, but the outcome is a piece of arts,\n");
-return (0);
+         prinf("with proper grammar, but the outcome is a piece of arts,\n");
+         return (0);
 }	
