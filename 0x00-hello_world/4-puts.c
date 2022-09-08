a428cdb79dbce6636ad23f967eab7eac4279f08e
Umohjoy <umohjoy27@gmail.com>
2022-09-08 14:16:07 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#!/bin/bash
+/**
+ * main - print string in the put function
+ * Description: using the main function
+ * this program prints "programming is like building a multiligual puzzle"
+ * Return 0
+ */
+int main(void)
+{
+puts("\"programming is like building a multilingual puzzle");
+return (0);
+}
