e987199b2e80632dac0c333e40d5570b1d22c1d5
Umohjoy <umohjoy27@gmail.com>
2022-09-08 19:07:30 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
 /**
- * main - print string in the function
- * but the outcome is an art piece
- * Description: using the main function
- * this program prints "programming is like building a multiligual puzzle
- * Return 0 if exited properly, non zero otherwise
- */
+* main - print string in the function
+*
+* Description: using the main function
+* this program prints "programming is like building a multiligual puzzle
+* Return 0 
+*/
 int main(void)
-{			
-         prinf("with proper grammar, but the outcome is a piece of arts,\n");
-         return (0);
+{ 
+prinf("with proper grammar, but the outcome is a piece of arts,\n");
+return (0);
 }	
