ab5b0845d0bec79037af668f3c940138d0663a18
Umohjoy <umohjoy27@gmail.com>
2022-09-08 20:46:58 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,11 +1,9 @@
 #include <stdio.h>
-
 /**
 * main - print the string and put function
-*
 * Description: using the main function
 * this program prints "Programming is like building a multilingual puzzle
-* Return 0
+* Return 0 if exited properly, non zero otherwise
 */
 int main(void)
 {
