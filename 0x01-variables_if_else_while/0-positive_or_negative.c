59e52f00bd261abe2b11db146e0482c133992a7f
Umohjoy <umohjoy27@gmail.com>
2022-09-09 10:05:28 -0700

    Testing

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,8 +1,9 @@
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
-
-/* betty style doc for function main goes there */
+/**
+ * main - Positive anything is better than negative nothing
+ * Return: 0
+ */
 int main(void)
 {
 	int n;
@@ -11,16 +12,16 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
 	if (n > 0)
-		{
+	{
 		printf("%d is postive\n", n);
-		}
-	else if(n == 0)
-		{
-		printf("%d is zero\n", n);
-		}
+	}
 	else if (n < 0)
-		{
+	{
 		printf("%d is negative\n", n);
-		}
+	}
+	else
+	{
+		printf("%d is zero\n", n);
+	}
 	return (0);
 }
