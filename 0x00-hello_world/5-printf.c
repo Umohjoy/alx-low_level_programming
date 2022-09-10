1d5c4f8bb03346b83bb42ae0a5c6eb4681cabf84
Umohjoy <umohjoy27@gmail.com>
2022-09-09 18:15:56 -0700

    5 printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
+
 /**
-* main - print string in the function
-*
-* Description: print out a sting to stdout.
-* Return: 0 if success.
-*/
+ * main - prints the phrase with proper grammar,
+ * but the outcome is a piece of art,
+ * Return: 0 if exited properly, non-zero otherwise
+ */
 int main(void)
-{ 
-prinf("with proper grammar, but the outcome is a piece of arts,\n");
-return (0);
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }	
