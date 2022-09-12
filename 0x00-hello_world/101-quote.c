44a2e5d7b2c105cf6dcc6211d5f89c3694252ed7
Umohjoy <umohjoy27@gmail.com>
2022-09-12 12:06:20 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,4 @@
 #include <unistd.h>
-
 /**
 *main - prints the standard error sandwiched around the standard output/t
 *
@@ -7,5 +6,7 @@
 */
 int main(void)
 {
-write(2,"and that piece of art is useful" - Dora Korpar, 2015-10-19\n',59);
-return (1); 
+write("and that piece of art is useful" - Dora Korpar, "2015-10-19\n");        
+return (1);
+
+}
