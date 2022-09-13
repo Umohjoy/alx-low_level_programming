73fcbee458249720ddf9532feec0e31b5fe5e7a5
Umohjoy <umohjoy27@gmail.com>
2022-09-13 13:24:15 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main-function that print alphabet in lowercase
+ * Return:(0)
+ * /
+int main(void)
+{
+char alphabet;
+for(c = 'a'; c <= 'z'; c++)
+{
+putchar(c);
+}
+putchar('\n');
+return (0);
+}
