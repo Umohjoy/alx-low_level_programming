ef04cb58db305a30a3a2546e51a06ff2cc263885
Umohjoy <umohjoy27@gmail.com>
2022-09-15 15:13:39 -0700

    2-largest_number

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 void postitive_or_negative(int i);
-int largest_number(int, int, int);
+int largest_number(int a; int b; int c;);
 void print_remaining_days(int, int,int);
 int convert_days(int, int);
 
