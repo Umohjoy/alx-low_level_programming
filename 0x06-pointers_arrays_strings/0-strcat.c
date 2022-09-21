e0aa9fd41061998201bf4d9e7453804c86a436eb
Umohjoy <umohjoy27@gmail.com>
2022-09-21 07:51:31 -0700

    strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* main - check the code
+* print_array - prints n elements of an array of integer
+* followed by a new line
+* @n: number of elements to print
+* Return: Always 0.
+*/
+char *_strcat(char *dest, char *src);
+{
+char str1[0] = "src";
+char str2[0] = "dest\0";
+char len
+len = strlen(str1);
+prinft("strlen(str1) : %d\n", len );
+return 0;
+}
