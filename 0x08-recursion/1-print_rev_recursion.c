bb9b9dfae01f147224a8d2737a4034ab43bc0c70
Umohjoy <umohjoy27@gmail.com>
2022-09-28 07:54:42 -0700

    1-print_rev_recursion.c

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,14 @@
+incude "main.h"
+
+/**
+* _print_rev_recursion - print a spring in reverse
+* @s : spring to be printed
+* Return: 0
+*/
+
+void _print_rev_recursion(char *s);
+{
+if (*s != '\0')
+_print_rev_recursion(s + 1);
+_putchar(*S);
+}
