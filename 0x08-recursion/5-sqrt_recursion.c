7c8ca83f6efe19b1aa1ccaa810dc324558a31d20
Umohjoy <umohjoy27@gmail.com>
2022-09-28 15:10:59 -0700

    5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -7,9 +7,9 @@
 */
 int _sqrt_recursion(int n)
 {
-if (n == 1 || n == 0)
-return (n);
-return (_sqrt(0, n));
+if (n < 0)
+return (-1);
+return (actual_sqrt_recursion(n, 0));
 }
 /**
 * _sqrt_recursion - returns the natural square root of a number
@@ -19,9 +19,9 @@ return (_sqrt(0, n));
 */
 int _sqrt(int n, int x)
 {
-if (n > x / 2)
+if (x * x > n)
 return (-1);
-else if (n * n == x)
-return (n);
-return (_sqrt(n + 1, x));
+if (x * x == n)
+return (x);
+return (actual_sqrt_recursion(n, x + 1));
 }
