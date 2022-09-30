575d00e99d0d6677dd11b3134af6d5c484c90fc9
Umohjoy <umohjoy27@gmail.com>
2022-09-30 15:38:04 -0700

    mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -13,7 +13,7 @@ i = 0;
 d = 0;
 n = 0;
 len = 0;
-f = 0;
+if = 0;
 digit = 0;
 while (s[len] != '\0')
 len++;
@@ -24,14 +24,17 @@ if (s[i] == '-')
 if (s[i] >= '0' && s[i] <= '9')
 {
 digit = s[i] - '0';
-if (d % 2) 
+if (d % 2)
 digit = -digit;
 n = n * 10 + digit;
-f = 1;									 if (s[i + 1] < '0' || s[i + 1] > '9')	
+f = 1;
+if (s[i + 1] < '0' || s[i + 1] > '9')
 break;
-if = (0);								 }
+f = 0;
+}
 i++;
-}									 if (f == 0)								  return (0);
+if (f == 0)
+return (0);
 return (n);
 }
 
@@ -46,7 +49,9 @@ int main(int argc, char *argv[])
 int result, num1, num2;
 if (argc < 3 || argc > 3)
 {
-printf("Error\n");							 return (1)								  }
+printf("Error\n");
+return (1);
+}
 num1 = _atoi(argv[1]);
 num2 = _atoi(argv[2]);
 result = num1 * num2;
