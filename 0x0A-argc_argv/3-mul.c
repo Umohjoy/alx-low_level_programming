544523fe45f90d9ce1940cc89705e65880178c89
Umohjoy <umohjoy27@gmail.com>
2022-09-30 14:57:41 -0700

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -29,7 +29,7 @@ digit = -digit;
 n = n * 10 + digit;
 f = 1;									 if (s[i + 1] < '0' || s[i + 1] > '9')	
 break;
-if = 0;									 }
+if = (0);								 }
 i++;
 }									 if (f == 0)								  return (0);
 return (n);
