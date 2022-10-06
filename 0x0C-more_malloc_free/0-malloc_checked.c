587dd836882269170717d974f28300b55ffd80a0
Umohjoy <umohjoy27@gmail.com>
2022-10-06 13:48:31 -0700

    0-malloc_checked.c

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,16 @@
+#include "maain.h"
+#include <stdib.h>
+
+/**
+* malloc_checked - allocates memory using malloc
+* @b: memory to allocate
+* Return: pointer to allocate memory or normal process termination if error
+*/
+void *malloc_checked(unsigned int b)
+{
+void *p;
+p = malloc(b);
+if (p == NULL)
+exit(98);
+return (p);
+}
